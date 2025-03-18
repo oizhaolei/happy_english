37f0668a45f89bc0be0784ae3d11654b37e90486
zhaolei <oizhaolei@gmail.com>
2025-03-18 19:02:57 +0900

    Initial commit from Create Next App
    prisma
    
    posts UI
    
    ignore env

---
+++ app/page.tsx
@@ -0,0 +1,68 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+import prisma from "@/lib/prisma";
+
+const Users = async () => {
+  const users = await prisma.user.findMany();
+  return users.map((user) => (
+    <li key={user.id} className="mb-2">
+      {user.name}
+    </li>
+  ));
+};
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Make&nbsp;</span>
+        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <br />
+        <span className={title()}>
+          websites regardless of your design experience.
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <Users />
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,19 @@
+import { Link } from "@heroui/link";
+import prisma from "@/lib/prisma";
+
+export default async function Posts() {
+  const posts = await prisma.post.findMany({
+    include: {
+      author: true,
+    },
+  });
+
+  return posts.map((post) => (
+    <li key={post.id}>
+      <span className="font-semibold">
+        <Link href={`/posts/${post.id}`}>{post.title}</Link>
+      </span>
+      <span className="text-sm text-gray-600 ml-2">by {post.author.name}</span>
+    </li>
+  ));
+}
